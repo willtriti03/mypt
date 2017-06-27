31c215802945e7630bb118c3f3b3b277524f7d45
jungjune <willtriti03@naver.com>
2017-06-28 08:32:07 +0900

    로그인화면  xml & 디자인

---
+++ mypt/app/src/main/java/com/example/jungjune/mypt/Activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.jungjune.mypt.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jungjune.mypt.R;
+
+/**
+ * Created by jungjune on 2017-06-27.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mypt/app/src/main/java/com/example/jungjune/mypt/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jungjune.mypt.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jungjune.mypt.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
