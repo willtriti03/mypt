699d696f43a8a40a7c5f06c5b454e09b2908393e
WILL\triti <willtriti03@naver.com>
2017-07-17 00:02:00 +0900

    이미지추가버튼

--- mypt/app/src/main/java/com/example/jungjune/mypt/Activity/SplashActicity.java
+++ mypt/app/src/main/java/com/example/jungjune/mypt/Activity/SplashActicity.java
@@ -1,11 +1,20 @@
 package com.example.jungjune.mypt.Activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.example.jungjune.mypt.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by triti on 2017-07-13.
@@ -13,7 +22,7 @@ import com.example.jungjune.mypt.R;
 
 public class SplashActicity extends Activity {
 
-
+    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1000;
     private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
@@ -21,14 +30,72 @@ public class SplashActicity extends Activity {
         super.onCreate(icicle);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActicity.this,LoginActivity.class);
-                SplashActicity.this.startActivity(mainIntent);
-                SplashActicity.this.finish();
+        int internetPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int storagePermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+        if (internetPermissionCheck == PackageManager.PERMISSION_DENIED || storagePermissionCheck == PackageManager.PERMISSION_DENIED) {
+            if (internetPermissionCheck == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.INTERNET},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
             }
-        }, SPLASH_DISPLAY_LENGTH);
+            if (storagePermissionCheck == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent mainIntent = new Intent(SplashActicity.this, LoginActivity.class);
+                    SplashActicity.this.startActivity(mainIntent);
+                    SplashActicity.this.finish();
+                }
+            }, SPLASH_DISPLAY_LENGTH);
+        }
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent mainIntent = new Intent(SplashActicity.this, LoginActivity.class);
+                            SplashActicity.this.startActivity(mainIntent);
+                            SplashActicity.this.finish();
+                        }
+                    }, SPLASH_DISPLAY_LENGTH);
+                } else {
+                    exitDialog();
+                }
+                return;
+        }
+    }
+
+    private void exitDialog() {
+        new AlertDialog.Builder(getApplicationContext())
+                .setTitle("권한 미설정") // 제목부분 텍스트
+                .setMessage("권한이 설정되지 않아 종료됩니다.") // 내용부분 텍스트
+                .setPositiveButton("예",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //프로그램 종료
+                                android.os.Process.killProcess(android.os.Process.myPid());
+                            }
+                        }
+                );
+
+    }
+
+
 }
--- mypt/app/src/main/java/com/example/jungjune/mypt/Fragment/TrainerMatchingFragment.java
+++ mypt/app/src/main/java/com/example/jungjune/mypt/Fragment/TrainerMatchingFragment.java
@@ -1,30 +1,89 @@
 package com.example.jungjune.mypt.Fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.jungjune.mypt.Item.MyPageItem;
 import com.example.jungjune.mypt.R;
 
 import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by triti on 2017-07-14.
  */
 
 public class TrainerMatchingFragment extends Fragment {
+    int GALLERYCODE = 444;
     View v;
     Context context;
+    ImageView imageView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         context=getActivity();
         v = inflater.inflate(R.layout.fragment_trainermatching, container, false);
+        imageView = (ImageView)v.findViewById(R.id.imageSlide) ;
+
+        Button addImage = (Button)v.findViewById(R.id.addImage);
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERYCODE);
+            }
+        });
         return v;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == -1) {
+            switch (requestCode) {
+                default:
+                    Uri imgUri = data.getData();
+                    String imagePath = getRealPathFromURI(imgUri);
+                    Glide.with(this).load(imagePath).into(imageView);
+                    break;
+            }
+
+        }
+
+    }
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getActivity().managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+
+    }
+
+
 }
